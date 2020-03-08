f556d93a777bf12724027d911cc83b958a2bdd29
ionutivan <ionut@brokoliapp.com>
2020-03-08 10:56:13 +0100

    Initial Commit

---
+++ Gitxing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitxing
+//
+//  Created by Ionut Ivan on 08/03/2020.
+//  Copyright © 2020 Ionut Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
